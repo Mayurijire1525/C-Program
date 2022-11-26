619e065cd07273d86cdc4f5a615cb28a90075f37
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 21:52:03 +0530

    If codeprogram20.c

---
+++ program20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	printf("Start main\n");
+	int x=10;
+	int y=20;
+
+	if(x){
+		printf("You enterd if block\n");
+	}
+	if(y){
+		printf("You entered if block\n");
+	}
+	printf("End main\n");
+}
