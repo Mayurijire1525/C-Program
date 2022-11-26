10b46eb6f9a736fefd033737faaa3bd665404046
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-26 21:18:06 +0530

    Unary codeprogram9.c

---
+++ program9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+	int x=5; 
+	int y=6;
+	int ans;
+
+	ans=x+y;
+	printf("%d\n",ans);
+
+	ans=y-x;
+	printf("%d\n",ans);
+
+	ans=x*y;
+	printf("%d\n",ans);
+
+	ans=x/y;
+	printf("%d\n",ans);
+
+	ans=x%y;
+	printf("%d\n",ans);
+
+	
+}
