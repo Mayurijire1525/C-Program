0d823b009689de2151640eb61a8a77893a4518d1
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-27 20:01:01 +0530

    1 to 9(3 by 3) for loop

---
+++ program37.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	
+	for(int row=1; row<=3; row++){
+		int num=1;
+		for(int col=1; col<=3; col++){
+			printf("%d ",num*num);
+			num++;
+			
+		}
+		printf("\n");
+	}
+}
