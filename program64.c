f1d957939c59f6e907ad83cd6b7abd94b89809cc
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-28 10:08:06 +0530

    symbol $ =(4 by 4) pattern

---
+++ program64.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	for(int row=1; row<=4; row++){
+		for(int col=1; col<=4; col++){
+			if(col%2){
+				printf("$ ");
+			}
+			else{
+				printf("= ");
+			}
+		}
+		printf("\n");
+	}
+}
