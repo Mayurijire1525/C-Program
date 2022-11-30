5c4484bdffa5573de74514dfd589533948e9f169
Mayurijire1525 <Mayurijire441@gmail.com>
2022-11-30 11:19:14 +0530

    array int sumprogram96.c

---
+++ program96.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int arr[5];
+	printf("Enter elements\n");
+	for(int i=0; i<5; i++){
+		scanf("%d",&arr[i]);
+	}
+	int sum=0;
+
+	for(int i=0; i<5; i++){
+		sum=sum+arr[i];
+	}
+	printf("Addition is=%d\n",sum);
+}
