5b7e82f2b1f7d167283d605b5222b2916386b844
Mayurijire1525 <Mayurijire441@gmail.com>
2022-12-03 10:53:14 +0530

    arr from userprogram118.c

---
+++ program118.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+	int size;
+	printf("Enter array size\n");
+	scanf("%d",&size);
+	int arr[size];
+
+	printf("Enter array element\n");
+	for(int i=0; i<size; i++){
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0; i<size; i++){
+		printf("%d\n",arr[i]);
+		printf("%d\n",*(arr+i));
+	}
+}
