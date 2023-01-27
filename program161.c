fdcad7af3136910e85acfccfd8bce28c022ad72d
Mayurijire1525 <Mayurijire441@gmail.com>
2023-01-27 18:12:49 +0530

    puts fun

---
+++ program161.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	char pname[]={'R','o','h','i','t'};
+	char*name="virat";
+	puts(pname);
+	puts(name);
+
+	printf("%ld\n",sizeof(pname));
+	printf("%ld\n",sizeof(name));
+
+}
