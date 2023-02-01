8d7373effc6034534ae2a13d6d22f77e9f9b44b2
Mayurijire1525 <Mayurijire441@gmail.com>
2023-02-01 09:52:14 +0530

    malloc

---
+++ program183.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int mallocadd(){
+	int *ptr1=(int*)malloc(sizeof(int));
+	int *ptr2=(int*)malloc(sizeof(int));
+
+	*ptr1=10;
+	*ptr2=20;
+	return *ptr1+*ptr2;
+}
+void main(){
+	int x=mallocadd();
+	printf("%d\n",x);
+}
